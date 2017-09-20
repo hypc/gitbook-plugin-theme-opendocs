afaea4766303b7e7dcb2c6b17525f4042f893d97
hypc <h_yp00@163.com>
2017-09-20 16:49:32 +0800

    update favicon.ico

--- src/build.sh
+++ src/build.sh
@@ -10,3 +10,4 @@ mkdir -p _assets/website/
 lessc -clean-css --include-path=$(node -e "console.log(require('gitbook-styleguide').less.paths.join(':'))") src/less/website.less _assets/website/style.css
 
 cp -r node_modules/gitbook-styleguide/static/* _assets/website/
+cp -r assets/* _assets/
